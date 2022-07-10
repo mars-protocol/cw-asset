ec9d1be60e34cb3abc6cfa74a906969718777a66
larry <26318510+larry0x@users.noreply.github.com>
2022-07-10 17:42:31 +0100

    formatting

--- src/asset_info.rs
+++ src/asset_info.rs
@@ -82,10 +82,9 @@ impl FromStr for AssetInfoUnchecked {
         match words[0] {
             "native" => {
                 if words.len() != 2 {
-                    return Err(StdError::generic_err(format!(
-                        "invalid asset info format `{}`; must be in format `native:{{denom}}`",
-                        s
-                    )));
+                    return Err(StdError::generic_err(
+                        format!("invalid asset info format `{}`; must be in format `native:{{denom}}`", s)
+                    ));
                 }
                 Ok(AssetInfoUnchecked::Native(String::from(words[1])))
             }
@@ -105,10 +104,9 @@ impl FromStr for AssetInfoUnchecked {
                 }
                 Ok(AssetInfoUnchecked::Cw1155(String::from(words[1]), String::from(words[2])))
             }
-            ty => Err(StdError::generic_err(format!(
-                "invalid asset type `{}`; must be `native` or `cw20` or `cw1155`",
-                ty
-            ))),
+            ty => Err(StdError::generic_err(
+                format!("invalid asset type `{}`; must be `native` or `cw20` or `cw1155`", ty)
+            )),
         }
     }
 }
